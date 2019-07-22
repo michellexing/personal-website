8e306a2565dc48ff979274bef4b7301ba20a7ca2
Michelle Xing <MiXing@Mac-4.local>
2019-07-21 18:57:37 -0700

    made resume page

---
+++ javascript/resume.js
@@ -0,0 +1,5 @@
+const download_btn = document.getElementById('download_button'); 
+
+download_btn.onclick = function() {
+    location.href = "https://drive.google.com/file/d/1h5akCgkuNK7_6IuqvjmgSHZnqYUQ5ZA5/view"; 
+};
